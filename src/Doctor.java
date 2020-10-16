e6f69e4fc5de8f3684511d15141b96702bf1fae8
Juan Torchia <j.s.torchia@gmail.com>
2020-10-15 23:21:47 -0300

    Generamos las clases anidadas a doctor para poder asignarle las citas que tiene el doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,11 +1,18 @@
 import javax.print.Doc;
+import java.util.ArrayList;
+import java.util.Date;
+
 
 public class Doctor {
     //Atributos
     static int id = 0; // Autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
+
+
+    //Available Appointment
+    private
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
@@ -25,4 +32,47 @@ public class Doctor {
         System.out.println("El id del medico asignado es: " + id);
     }
 
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){
+        availableAppointments.add(new Doctor.AvailableAppointment(date,time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
+    }
+
+    public static class AvailableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 
 import static ui.UiMenu.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         //showMenu();
-        Doctor myDoctor = new Doctor("Juan Torchia","Psicologo");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
 
-        //
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
 
+
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        /*
         Patient patient = new Patient("Enrique","enrique@hospital.ar");
         patient.setWeight(54.5);
         System.out.println(patient.getWeight());
         patient.setPhoneNumber("1123872075");
         System.out.println(patient.getPhoneNumber());
-
+        */
 
 
     }
